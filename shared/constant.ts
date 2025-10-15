e76963cdca6526f82380aa06d56c1496f932a190
Hank Yang <86253725+SheepNDW@users.noreply.github.com>
2025-10-15 17:46:39 +0800

    feat: add agents constant with names and pictures

---
+++ shared/constant.ts
@@ -0,0 +1,214 @@
+import type { Agent } from './types';
+
+export const AGENTS = new Map<string, Agent>([
+  [
+    '🐷',
+    {
+      name: '泠玲',
+      picture: 'https://o8ilaibv5w.ufs.sh/f/Q681AB1tpzcuLy1qse9mYTlDH32ZF0nMIWydusApvaojBGEb',
+    },
+  ],
+  [
+    '🥨',
+    {
+      name: '米捲',
+      picture: 'https://o8ilaibv5w.ufs.sh/f/Q681AB1tpzcu7J0xbXvvpE8xoHPWJ9UdMK4hqGBQZDu0XmAN',
+    },
+  ],
+  [
+    '🌙',
+    {
+      name: 'Luna',
+      picture: 'https://o8ilaibv5w.ufs.sh/f/Q681AB1tpzcuzacqGE5HmVSe98IXu62QYspBgGU51Owt0P3c',
+    },
+  ],
+  [
+    'Ruby',
+    {
+      name: 'Ruby',
+      picture: 'https://o8ilaibv5w.ufs.sh/f/Q681AB1tpzcuCBHoQf34YAXeLkv6NxtVOjMJsu01RWdITqoB',
+    },
+  ],
+  [
+    '梂',
+    {
+      name: '梂',
+      picture: 'https://o8ilaibv5w.ufs.sh/f/Q681AB1tpzcuWEfQoDpc4b0vXsr7VhFqM9ZnLipjyfu2mIAN',
+    },
+  ],
+  [
+    '紅',
+    {
+      name: '紅',
+      picture: 'https://o8ilaibv5w.ufs.sh/f/Q681AB1tpzcu89N0JuMnaK4tR7Jbfz0B9DuAdOjWgeEI5lcH',
+    },
+  ],
+  [
+    '理音',
+    {
+      name: '理音',
+      picture: 'https://o8ilaibv5w.ufs.sh/f/Q681AB1tpzcuyjpw5fq0Ng8FJVKWE2xCmQZq4jUXIdpbiyS7',
+    },
+  ],
+  [
+    '百夜',
+    {
+      name: '百夜',
+      picture: 'https://o8ilaibv5w.ufs.sh/f/Q681AB1tpzcuNo5lFU88pmAd1J73OGKcWTRwZxlv4CI9kyLg',
+    },
+  ],
+  [
+    '凜奈',
+    {
+      name: '凜奈',
+      picture: 'https://o8ilaibv5w.ufs.sh/f/Q681AB1tpzcuWJA9nSpc4b0vXsr7VhFqM9ZnLipjyfu2mIAN',
+    },
+  ],
+  [
+    '千熊',
+    {
+      name: '千熊',
+      picture: 'https://o8ilaibv5w.ufs.sh/f/Q681AB1tpzcu6KxRdxfJAvDsOl9UNkiP7qSKXjIuRWradT06',
+    },
+  ],
+  [
+    '熊子',
+    {
+      name: '熊子',
+      picture: 'https://o8ilaibv5w.ufs.sh/f/Q681AB1tpzcuBNj0lPVW0tLQAXzj1cFSCJk7agwsuPDWmhxq',
+    },
+  ],
+  [
+    'Kikimi',
+    {
+      name: 'Kikimi',
+      picture: 'https://o8ilaibv5w.ufs.sh/f/Q681AB1tpzcuBCG3upW0tLQAXzj1cFSCJk7agwsuPDWmhxqZ',
+    },
+  ],
+  [
+    '小楓',
+    {
+      name: '小楓',
+      picture: 'https://o8ilaibv5w.ufs.sh/f/Q681AB1tpzcuZ8qdjuaceCOgjTpr60Y3f8w4unidbFXvkB9l',
+    },
+  ],
+  [
+    '音',
+    {
+      name: '音',
+      picture: 'https://o8ilaibv5w.ufs.sh/f/Q681AB1tpzcuuK2cuXClhNn8bH5me2PWRkg9GVoL1q6viOCw',
+    },
+  ],
+  [
+    '花緒',
+    {
+      name: '花緒',
+      picture: 'https://o8ilaibv5w.ufs.sh/f/Q681AB1tpzcuQfQDiPtpzcu0lMI5wN6vjb9iD3GyHLY1anXJ',
+    },
+  ],
+  [
+    '三里',
+    {
+      name: '三里',
+      picture: 'https://o8ilaibv5w.ufs.sh/f/Q681AB1tpzcu81F7gUUMnaK4tR7Jbfz0B9DuAdOjWgeEI5lc',
+    },
+  ],
+  [
+    '井野',
+    {
+      name: '井野',
+      picture: 'https://o8ilaibv5w.ufs.sh/f/Q681AB1tpzcuUpzp9PkBHCabipIoGJADkslqFegBjxv6QEnc',
+    },
+  ],
+  [
+    '小春',
+    {
+      name: '小春',
+      picture: 'https://o8ilaibv5w.ufs.sh/f/Q681AB1tpzcu1QJxdNFQhXuDmRLFk4l9Zyps5I7ztcgBGf6N',
+    },
+  ],
+  [
+    '綾音',
+    {
+      name: '綾音',
+      picture: 'https://o8ilaibv5w.ufs.sh/f/Q681AB1tpzcu0SpbxJyn8Sp9RqhgC4y0juW5saEdQbxvM7NI',
+    },
+  ],
+  [
+    '棠棠',
+    {
+      name: '棠棠',
+      picture: 'https://o8ilaibv5w.ufs.sh/f/Q681AB1tpzcuWE8nrzpc4b0vXsr7VhFqM9ZnLipjyfu2mIAN',
+    },
+  ],
+  [
+    '明里',
+    {
+      name: '明里',
+      picture: 'https://o8ilaibv5w.ufs.sh/f/Q681AB1tpzcuTrvkSAdA95N1PDLEqaCVyK37J0cOZt8mQXIn',
+    },
+  ],
+  [
+    '酒玖',
+    {
+      name: '酒玖',
+      picture: 'https://o8ilaibv5w.ufs.sh/f/Q681AB1tpzcuiLiBp1xAFjVWdv9lIeQuasrSZ3bRhx80gimC',
+    },
+  ],
+  [
+    '璐奈',
+    {
+      name: '璐奈',
+      picture: 'https://o8ilaibv5w.ufs.sh/f/Q681AB1tpzcuchDyYcnjQYKVGoj2PySknsNLbWCRdxtmHIq8',
+    },
+  ],
+  [
+    '景子',
+    {
+      name: '景子',
+      picture: 'https://o8ilaibv5w.ufs.sh/f/Q681AB1tpzcumoIWb52jqZfLa4Yz1d2UTnO3AV6G7cpgyiHw',
+    },
+  ],
+  [
+    '莉央',
+    {
+      name: '莉央',
+      picture: 'https://o8ilaibv5w.ufs.sh/f/Q681AB1tpzcudNlWqsY9QEtrNiWVku3w0MsvybnKR6aXFlOe',
+    },
+  ],
+  [
+    '和実',
+    {
+      name: '和実',
+      picture: 'https://o8ilaibv5w.ufs.sh/f/Q681AB1tpzcuKhFBIKgXkOU7gfRyl8YI1w4WhNSZedJjCHbc',
+    },
+  ],
+  [
+    '子子',
+    {
+      name: '子子',
+      picture: 'https://o8ilaibv5w.ufs.sh/f/Q681AB1tpzcumUzNLq2jqZfLa4Yz1d2UTnO3AV6G7cpgyiHw',
+    },
+  ],
+  [
+    '芽',
+    {
+      name: '芽',
+      picture: 'https://o8ilaibv5w.ufs.sh/f/Q681AB1tpzcuddvu6lY9QEtrNiWVku3w0MsvybnKR6aXFlOe',
+    },
+  ],
+  [
+    '亞米',
+    {
+      name: '亞米',
+      picture: 'https://o8ilaibv5w.ufs.sh/f/Q681AB1tpzcunmcVNlm1zw2bvJEWMYq7dnLm0r3cflgZeSRs',
+    },
+  ],
+  [
+    '七尾',
+    {
+      name: '七尾',
+      picture: 'https://o8ilaibv5w.ufs.sh/f/Q681AB1tpzcuCUMTJa34YAXeLkv6NxtVOjMJsu01RWdITqoB',
+    },
+  ],
+]);
